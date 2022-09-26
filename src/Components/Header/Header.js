9d813404fb0085489c597744c648df6aedadcb14
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-09-26 19:36:33 +0600

    header section added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
         <div className='navbar'>
             <img src={Logo} alt="" />
             <nav className='nav-item'>
-                <a href="/Oreder">Order</a>
                 <a href="/Home">Home</a>
+                <a href="/Oreder">Order</a>
                 <a href="/About">About</a>
                 <a href="/Login">Login</a>
             </nav>
